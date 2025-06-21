32ed87b4ff598f59b4961788872992d64deb6790
Edgard Cuadra <edgardcu@gmail.com>
2025-06-20 23:00:44 -0400

    Update firebase-config.js
    modern ES6 module export

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 // Firebase Configuration
-// This file contains your Firebase project settings
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBIJ6KUPeGSHvHWwz5ncRLClhh74kMQDa8",
-  authDomain: "workouttracker-e1688.firebaseapp.com",
-  projectId: "workouttracker-e1688",
-  storageBucket: "workouttracker-e1688.firebasestorage.app",
-  messagingSenderId: "948286237385",
-  appId: "1:948286237385:web:35669c5a8fd3beb76b7b18",
-  measurementId: "G-NJ7JDVLLEQ"
+export const firebaseConfig = {
+    apiKey: "AIzaSyBIJ6KUPeGSHvHWwz5ncRLClhh74kMQDa8",
+    authDomain: "workouttracker-e1688.firebaseapp.com",
+    projectId: "workouttracker-e1688",
+    storageBucket: "workouttracker-e1688.firebasestorage.app",
+    messagingSenderId: "948286237385",
+    appId: "1:948286237385:web:35669c5a8fd3beb76b7b18",
+    measurementId: "G-NJ7JDVLLEQ"
 };
